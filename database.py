6d45dcbea08daf70bdad74891a59023b83fdc68c
Wojciech Konsik <wojciechkonsik@device-2248.home>
2026-07-19 18:45:46 +0200

    Created second db ticket

--- database.py
+++ database.py
@@ -10,6 +10,21 @@ cursor.execute("CREATE TABLE IF NOT EXISTS users (id bigint GENERATED ALWAYS AS 
                "active boolean DEFAULT true,"
                "created timestamptz DEFAULT now())")
 
+cursor.execute("CREATE TABLE IF NOT EXISTS tickets (id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,"
+               "title TEXT,"
+               "description TEXT,"
+               "status TEXT CHECK (status IN ('OPEN', 'IN_PROGRESS', 'CLOSED')),"
+               "priority INTEGER, "
+               "category TEXT,"
+               "author_id INTEGER,"
+               "agent_id INTEGER, "
+               "created timestamptz DEFAULT now(),"
+               "updated timestamptz,"
+               "closed timestamptz, "
+               ""
+               "FOREIGN KEY (author_id) REFERENCES users(id),"
+               "FOREIGN KEY (agent_id) REFERENCES users(id) )")
+
 connection.commit()
 cursor.close()
 connection.close()
