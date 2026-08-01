47877ed188b137d67fc00cb293963ce12742baa7
Tojt1 <konsikwojciech@onet.pl>
2026-08-02 00:50:40 +0200

    better catching errors in repository and new errors

--- tickets/repository.py
+++ tickets/repository.py
@@ -3,79 +3,104 @@ import datetime
 import exceptions
 
 
-
-
 def db_exists_ticket(ticket_id):
     conn = pool.getconn()
-    with conn.cursor() as cur:
-        cur.execute("SELECT title FROM tickets WHERE id =%s", (ticket_id, ))
-        return cur.fetchone()
 
-    pool.putconn()
+    with conn.cursor() as cur:
+        try:
+            cur.execute("SELECT title FROM tickets WHERE id =%s", (ticket_id, ))
+            return cur.fetchone()
+
+        except Exception:
+            raise exceptions.DBCehckExistsError("Wystąpił błąd podczas sprawdzania ticketu")
+
+        finally:
+            pool.putconn()
 
 def check_if_ticket_close(ticket_id):
     conn = pool.getconn()
-    with conn.cursor() as cur:
-        cur.execute("SELECT status from tickets WHERE id = %s", (ticket_id, ))
-        return cur.fetchone()
 
-    pool.putconn()
+    with conn.cursor() as cur:
+        try:
+            cur.execute("SELECT status from tickets WHERE id = %s", (ticket_id, ))
+            return cur.fetchone()
+
+        except Exception:
+            raise exceptions.DBCehckExistsError("wystąpił błąd podczas sprawdzania, czy ticket jest zamknięty")
+
+        finally:
+            pool.putconn()
 
 def assign_agent(ticket_id, user_id):
     conn = pool.getconn()
+
     with conn.cursor() as cur:
         try:
             cur.execute("UPDATE tickets SET status ='IN_PROGRESS', agent_id = %s, updated = %s WHERE id = %s", (user_id, datetime.datetime.now(), ticket_id))
             conn.commit()
-            return {"information": "Pomyślnie dodano agenta do ticketa"}
-        except exceptions.DbAddError:
-            return {"error": "wystąpił błą∂ podczas dodawania agenta"}
+            return True
+        except Exception:
+            conn.rollback()
+            raise exceptions.DBAssignAgentError("Wystąpił błąd podczas przypisywania agenta do ticketa ")
 
         finally:
             pool.putconn(conn)
 
 def add_ticket(ticket, user_id):
     conn = pool.getconn()
+
     with conn.cursor() as cur:
         try:
-            cur.execute("INSERT INTO tickets(title, description, priority, category, author_id) VALUES(%s, %s, %s, %s, %s)", (ticket.title, ticket.description, ticket.priority, ticket.category, user_id))
+            cur.execute("INSERT INTO tickets(title, description, priority, category, author_id) VALUES(%s, %s, %s, %s, %s)",
+                        (ticket.title, ticket.description, ticket.priority, ticket.category, user_id))
             conn.commit()
-            return {"information": "Pomyślnie utworzono ticket"}
-        except exceptions.DbAddError:
-            return {"error":"podano nieprawidlowoa wartosc"}
+            return True
+
+        except Exception:
+            conn.rollback()
+            raise exceptions.DbAddError("Wystąpił błąd podczas tworzenia ticketa")
+
         finally:
             pool.putconn(conn)
 
 def get_ticket(ticket_id):
     conn = pool.getconn()
+
     with conn.cursor() as cur:
         try:
-            cur.execute(
-                "SELECT * FROM tickets WHERE id =%s", (ticket_id, ))
+            cur.execute("SELECT * FROM tickets WHERE id =%s", (ticket_id, ))
             return cur.fetchone()
-        except exceptions.DbDownloadError:
-            return {"error": "wystąpił błąd podczas pobierania danych"}
+
+        except Exception:
+            raise exceptions.DbDownloadError("Wystąpił błąd poczas pobierania ticketu")
+
         finally:
             pool.putconn(conn)
 
 def get_tickets_by_status(status, sort,  limit, offset):
     conn = pool.getconn()
+
     with conn.cursor() as cur:
         try:
             cur.execute(f"SELECT * FROM tickets WHERE status = %s ORDER BY {sort}  LIMIT %s OFFSET %s", (status, limit, offset ))
             return cur.fetchall()
-        except exceptions.DbDownloadError:
-            return {"error": "wystąpił błąd podczas pobierania danych"}
+
+        except Exception:
+            raise exceptions.DbDownloadError("Wystąpił błąd podczas pobierania ticketów za pomoca statusu")
+
         finally:
             pool.putconn(conn)
 
 def get_all_tickets(limit, offset, sort):
     conn = pool.getconn()
+
     with conn.cursor() as cur:
         try:
             cur.execute(f"SELECT * FROM tickets ORDER BY {sort} DESC LIMIT %s OFFSET %s", (limit, offset))
             return cur.fetchall()
-        except exceptions.DbDownloadError:
-            return {"error": "wystąpił błąd podczas pobierania danych"}
+
+        except Exception:
+            raise exceptions.DbDownloadError("Wystąpił błąd podczas pobierania ticketów")
+
         finally:
             pool.putconn(conn)
--- users/repository.py
+++ users/repository.py
@@ -1,61 +1,105 @@
 from database import  pool
+import exceptions
 
 
 def create_user(user, hashed_password):
     conn = pool.getconn()
+
     try:
         with conn.cursor() as cur:
             cur.execute("INSERT INTO users(name, email, password) VALUES(%s, %s, %s)", (user.name, user.email, hashed_password))
             conn.commit()
-        return {"information": "Pomyślnie utworzono użytkownika"}
-    except ValueError:
-        return {"error": "Wystąpił porblem podczas tworzenia"}
+            return True
+
+    except Exception:
+        conn.rollback()
+        raise exceptions.DbAddError("Wystąpił błąd podczas dodawania użytkownika")
+
     finally:
         pool.putconn(conn)
 
 def check_user_email(email):
     conn = pool.getconn()
+
     with conn.cursor() as cur:
-        cur.execute("SELECT id FROM users WHERE email = %s", (email,))
-        return cur.fetchone()
-    pool.putconn(conn)
+        try:
+            cur.execute("SELECT id FROM users WHERE email = %s", (email,))
+            return cur.fetchone()
+
+        except Exception:
+            raise exceptions.DBCehckExistsError("Wystąpił błąd podczas sprawdzania email użytkownika")
+
+        finally:
+            pool.putconn(conn)
 
 def check_user_password(email):
     conn = pool.getconn()
+
     with conn.cursor() as cur:
-        cur.execute("SELECT password, id FROM users WHERE email = %s", (email,))
-        return cur.fetchone()
-    pool.putconn(conn)
+        try:
+            cur.execute("SELECT password, id FROM users WHERE email = %s", (email,))
+            return cur.fetchone()
+
+        except Exception:
+            raise exceptions.DBCehckExistsError("Wystąpił błąd podczas sprawdzania hasła użytkownika")
+        finally:
+            pool.putconn(conn)
 
 def get_all_users():
     conn = pool.getconn()
+
     with conn.cursor() as cur:
-        cur.execute("SELECT id, name, role FROM users")
-        return cur.fetchall()
-    pool.putconn()
+        try:
+            cur.execute("SELECT id, name, role FROM users")
+            return cur.fetchall()
+
+        except Exception:
+            raise exceptions.DbDownloadError("Wystąpił błąd podczas pobierania użytkowniowk")
+
+        finally:
+            pool.putconn()
 
 def get_id(email):
     conn = pool.getconn()
+
     with conn.cursor() as cur:
-        cur.execute("SELECT id FROM users WHERE email = %s", (email, ))
-        return cur.fetchone()
-    pool.putconn(conn)
+        try:
+            cur.execute("SELECT id FROM users WHERE email = %s", (email, ))
+            return cur.fetchone()
+
+        except Exception:
+            raise exceptions.GetUserIdError("Wystąpił błąd podczas pobierania ID użytkownika ")
+
+        finally:
+            pool.putconn(conn)
 
 def download_user_info(id):
     conn = pool.getconn()
-    with conn.cursor() as cur:
-        cur.execute("SELECT name, email, role, active, created FROM users WHERE id = %s", (id, ))
-        return cur.fetchone()
 
-    pool.putconn(conn)
+    with conn.cursor() as cur:
+        try:
+            cur.execute("SELECT name, email, role, active, created FROM users WHERE id = %s", (id, ))
+            return cur.fetchone()
+
+        except Exception:
+            raise exceptions.DbDownloadError("Wystąpił błąd podczas pobierania informacji użytkownika")
+
+        finally:
+            pool.putconn(conn)
 
 def get_user_role(user_id):
     conn = pool.getconn()
-    with conn.cursor() as cur:
-        cur.execute("SELECT role FROM users WHERE id = %s", (user_id, ))
-        return cur.fetchone()
 
-    pool.putconn(conn)
+    with conn.cursor() as cur:
+        try:
+            cur.execute("SELECT role FROM users WHERE id = %s", (user_id, ))
+            return cur.fetchone()
+
+        except Exception:
+            raise exceptions.DBCehckExistsError("Wystąpił błąd podczas pobierania roli użytkownika")
+
+        finally:
+            pool.putconn(conn)
 
 
 
