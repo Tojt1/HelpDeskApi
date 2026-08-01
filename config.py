c9e6a2a8126622ab6f48396bc25182808bc85bf0
Tojt1 <konsikwojciech@onet.pl>
2026-08-02 00:04:29 +0200

    make .env file for more security aproach

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
-SECRET_KEY = "Bardzo_tajne_haslo"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = os.getenv('DB_HOST')
+USER = os.getenv('DB_USER')
+PASSWORD = os.getenv('DB_PASSWORD')
+NAME = os.getenv('DB_NAME')
+
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
