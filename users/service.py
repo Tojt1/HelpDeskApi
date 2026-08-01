1c4a99c372a857bc85f9fe95917b6037b104a3f8
Tojt1 <konsikwojciech@onet.pl>
2026-08-01 17:49:37 +0200

    made a litle bit better validation

--- users/service.py
+++ users/service.py
@@ -5,11 +5,14 @@ import config
 import exceptions
 
 def register_user(user):
-    valid_email(user.email)
-    ensuer_email_not_exists(user.email)
-    valid_password(user.password)
-    hashed_password = hash_password(user.password)
-    repository.create_user(user, hashed_password)
+    try:
+        valid_email(user.email)
+        ensuer_email_not_exists(user.email)
+        valid_password(user.password)
+        hashed_password = hash_password(user.password)
+        repository.create_user(user, hashed_password)
+    except:
+        raise ValueError("Wystąpił błąd podczas logowania")
 
 def valid_password(password):
     special_chars = "!@#$%^&*()_+-=[]{}|;':\",.<>/?"
@@ -28,7 +31,7 @@ def valid_password(password):
 
     return None
 
-def hash_password(password):
+def hash_password(password:str)->str:
 
     bpassword = password.encode("utf-8")
     hashed = bcrypt.hashpw(bpassword, bcrypt.gensalt())
@@ -39,7 +42,7 @@ def check_password(password:str, hashed_password:str) ->bool:
     bpassword = password.encode("utf-8")
     return bcrypt.checkpw(bpassword, hashed_password.encode("utf-8"))
 
-def valid_email(email):
+def valid_email(email:str):
     if "@" not in email:
         return {"error": "email jest nieprawidłowy"}
 
@@ -69,7 +72,7 @@ def load_all_users():
 
 
 
-def create_token(id):
+def create_jwt_toc(id):
     token = jwt.encode(
         {"id": id[1]},
         config.SECRET_KEY,
@@ -86,7 +89,7 @@ def login_user(user):
     valid_email(user.email)
     result = ensuer_email_exists(user.email)
     if check_password(user.password, result[0]):
-        return create_token(result)
+        return create_jwt_toc(result)
     else:
         raise exceptions.InvalidPasswordError("Podano niepoprawne hasło")
 
@@ -101,7 +104,7 @@ def user_info(token:str):
         "created": user[4]
     }
 
-def check_user_admin(user_id):
+def check_user_admin(user_id)->bool:
     if repository.get_user_role(user_id)[0] == "ADMIN":
         return True
     return False
