c9466a42f87d2f51720d59fd1fd4e44c9fefaeee
Tojt1 <konsikwojciech@onet.pl>
2026-08-15 00:59:56 +0200

    create files and made simply login

--- frontend/src/components/Tickets.jsx
+++ frontend/src/components/Tickets.jsx
@@ -1,5 +1,19 @@
+
 function ShowTickets({ticket}){
+    function AssignTo(){
+        alert("clicked")
+    }
     return (
-        <div className="tickets"></div>
+        <div className="tickets">
+            <div className="ticket-info">
+                <h3>{ticket.title}</h3>
+                <p>{ticket.created}</p>
+            </div>
+            <div className="ticket-overlay">
+                <button className="assign-btn" onClick={AssignTo}>Przypisz</button>
+            </div>
+        </div>
     )
 }
+
+export default  ShowTickets
