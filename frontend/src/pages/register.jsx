233b89331061b6de66b6fcc26e5121aafbb98c77
Tojt1 <konsikwojciech@onet.pl>
2026-08-16 20:01:00 +0200

    before making ticket, make sure jwt token exists

---
+++ frontend/src/pages/register.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+
+
+function Register (){
+
+    return(
+        <div>
+
+        </div>
+    )
+}
--- users/router.py
+++ users/router.py
@@ -28,7 +28,7 @@ def sign_up(user:RegisterUser):
 @router_user.post("/login")
 def  sign_in(user:LoginUser):
     try:
-        return users.service.login_user(user)
+        return {"token":users.service.login_user(user)}
     except exceptions.UserLoginError as e:
         return HTTPException(
             status_code=400,
