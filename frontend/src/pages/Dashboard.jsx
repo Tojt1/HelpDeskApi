cffa27006a645b47f7ff2e1a288524a6a3efbea9
Tojt1 <konsikwojciech@onet.pl>
2026-08-19 00:00:29 +0200

    better view for dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,7 @@
 import {jwtDecode} from "jwt-decode";
 import {useState} from "react";
+import Usermenu from "../components/Usermenu.jsx";
+import "./Dashboard.css"
 
 function Dashboard(){
     const token = localStorage.getItem("token");
@@ -29,36 +31,40 @@ function Dashboard(){
     }
 
     return(
-        <div className={"dasboard-container"}>
+        <>
+            <Usermenu />
 
-            <h2>Hello {users.name}</h2>
+            <div className="dasboard-container">
 
-            <form onSubmit={SubmitDash} className="dashboard-from">
+                <h2>Hello {users.name}</h2>
 
-                <p>Jakiego rodzaju jest twój problem?</p>
-                <input
-                type="text"
-                placeholder="....."
-                value={problemTitle}
-                onChange={(e) => setProblemTitle(e.target.value)}/>
+                <form onSubmit={SubmitDash} className="dashboard-form">
 
-                <p>Opisz swój problem</p>
-                <input
-                type="text"
-                placeholder="....."
-                value={problemDescription}
-                onChange={(e) => setProblemDescription(e.target.value)}/>
+                    <p>Jakiego rodzaju jest twój problem?</p>
+                    <input
+                    type="text"
+                    placeholder="....."
+                    value={problemTitle}
+                    onChange={(e) => setProblemTitle(e.target.value)}/>
 
-                <p>Z jakiej kategori jest twój problem</p>
-                <input
-                type="text"
-                placeholder="Category...."
-                value={problemCategory}
-                onChange={(e)=> setProblemCategory(e.target.value)}/>
+                    <p>Opisz swój problem</p>
+                    <input
+                    type="text"
+                    placeholder="....."
+                    value={problemDescription}
+                    onChange={(e) => setProblemDescription(e.target.value)}/>
 
-                <button type="submit" className="dashboard-btn">Wyślij ticket</button>
-            </form>
-        </div>
+                    <p>Z jakiej kategori jest twój problem</p>
+                    <input
+                    type="text"
+                    placeholder="Category...."
+                    value={problemCategory}
+                    onChange={(e)=> setProblemCategory(e.target.value)}/>
+
+                    <button type="submit" className="dashboard-btn">Wyślij ticket</button>
+                </form>
+            </div>
+        </>
     )
 }
 
