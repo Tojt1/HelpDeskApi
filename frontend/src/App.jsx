09176304956ad4b4f16146115dbe7148b8a94c47
Tojt1 <konsikwojciech@onet.pl>
2026-08-18 23:22:53 +0200

    crete view for logged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/login.jsx";
 import Home from "./pages/Home.jsx";
 import RegisterUser from "./pages/register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import CheckLogged from "./components/CheckLogged.jsx";
 import { Routes, Route} from "react-router";
 
 function App() {
@@ -12,7 +13,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<RegisterUser />}/>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={
+              <CheckLogged>
+                  <Dashboard />
+              </ CheckLogged>
+              } />
       </Routes>
   )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,63 @@
 import {jwtDecode} from "jwt-decode";
+import {useState} from "react";
 
 function Dashboard(){
     const token = localStorage.getItem("token");
     const users = jwtDecode(token);
+    const [problemTitle, setProblemTitle] = useState("")
+    const [problemDescription, setProblemDescription] = useState("")
+    const [problemCategory, setProblemCategory] = useState("")
+
+    const SubmitDash = async (e) => {
+        e.preventDefault()
+
+        const token = localStorage.getItem("token")
+
+        const response = await fetch("http://localhost:8000/tickets", {
+            method: "POST",
+            headers: {
+                "Content-Ype": "application/json",
+                "Authorisation": `Bearer ${token}`
+            },
+            body:JSON.stringify({
+                title: problemTitle,
+                description: problemDescription,
+                category: problemCategory
+            })
+        })
+        const data = response.json()
+    }
 
     return(
-        <div>
+        <div className={"dasboard-container"}>
+
             <h2>Hello {users.name}</h2>
+
+            <form onSubmit={SubmitDash} className="dashboard-from">
+
+                <p>Jakiego rodzaju jest twój problem?</p>
+                <input
+                type="text"
+                placeholder="....."
+                value={problemTitle}
+                onChange={(e) => setProblemTitle(e.target.value)}/>
+
+                <p>Opisz swój problem</p>
+                <input
+                type="text"
+                placeholder="....."
+                value={problemDescription}
+                onChange={(e) => setProblemDescription(e.target.value)}/>
+
+                <p>Z jakiej kategori jest twój problem</p>
+                <input
+                type="text"
+                placeholder="Category...."
+                value={problemCategory}
+                onChange={(e)=> setProblemCategory(e.target.value)}/>
+
+                <button type="submit" className="dashboard-btn">Wyślij ticket</button>
+            </form>
         </div>
     )
 }
