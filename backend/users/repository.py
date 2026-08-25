0894e3318129461a0af87d06cec74036e697bfa6
Tojt1 <konsikwojciech@onet.pl>
2026-08-25 20:17:44 +0200

    posibility to change email

--- backend/users/repository.py
+++ backend/users/repository.py
@@ -103,5 +103,19 @@ def get_user_role(user_id):
         finally:
             pool.putconn(conn)
 
+def change_email(user_id, new_email):
+    conn = pool.getconn()
+
+    with conn.cursor as cur:
+        try:
+            cur.execute("UPDATE users SET email = %s WHERE id = %s ", (new_email, user_id))
+            conn.commit()
+            return True
+        except Exception as e:
+            print("Error: ", e)
+            raise exceptions.ChangeEmailError("Wystąpił błąd podczas zmiany e-maila")
+
+        finally:
+            pool.putconn(conn)
 
 
