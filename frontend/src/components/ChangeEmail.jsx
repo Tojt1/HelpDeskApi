8da6f6a1894beab86bf120e661de5e364e14aed0
Tojt1 <konsikwojciech@onet.pl>
2026-08-25 20:18:39 +0200

    inputs for change email

--- frontend/src/components/ChangeEmail.jsx
+++ frontend/src/components/ChangeEmail.jsx
@@ -3,6 +3,14 @@ import "./ChangeEmail.css"
 
 function ChangeEmail(){
     const [open, setOpen] = useState(false)
+    const [currentEmail, setCurrentEmail] = useState("")
+    const [newEmail, setNewEmail] = useState("")
+
+    const handdleChangeEmail = async (e) => {
+        e.preventDefault()
+
+        const token = localStorage.getItem("token")
+    }
 
 
     return (
@@ -16,9 +24,24 @@ function ChangeEmail(){
                 <div className="modal"
                      onClick={(e) => e.stopPropagation()}
                 >
+                    <h2>Zmień email:</h2>
+
+                    <p>Obecny email::</p>
+                    <input
+                    type="text"
+                    placeholder="Current email.... "
+                    value={currentEmail}
+                    onChange={(e) => setCurrentEmail(e.target.value)}/>
+
+                    <p>nowy email:</p>
+                    <input
+                    type="text"
+                    placeholder="new email.... "
+                    value={newEmail}
+                    onChange={(e) => setNewEmail(e.target.value)}/>
+
                     <button onClick={() => setOpen(true)}>Zmień</button>
 
-                    <h2>Hello</h2>
                 </div>
 
             </div>
