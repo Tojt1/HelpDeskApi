f1c0e56eb8c3f4b3d5547f46d41409ad43fad650
Tojt1 <konsikwojciech@onet.pl>
2026-08-27 18:51:18 +0200

    make change password window work

--- frontend/src/components/ChangePassword.jsx
+++ frontend/src/components/ChangePassword.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import "./ChangePassword.css"
 
 function ChangePassword() {
     const [open, setOpen] = useState(false)
@@ -11,39 +12,45 @@ function ChangePassword() {
         const token = localStorage.getItem("token")
 
         const response = await fetch("http://localhost:8000/me/password", {
-            method: "Post",
+            method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${token}`
             },
             body: JSON.stringify({
-                new_password: newPassword
+                new_password: newPassword,
+                old_password: currentPassword
             })
         })
+        const data = response.json()
+        console.log("t")
     }
 
     return (
         <>
             <button onClick={() => setOpen(true)}>Zmień</button>
-            <div className="model-background" onClick={() => setOpen(false)}></div>
 
-            {open &&
-            <div className="model" onClick={(e) => e.stopPropagation()}>
-                <p>Obecne hasło: </p>
-                <input
-                type="text"
-                placeholder="Current password..."
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}/>
-                <p>Nowe hasło: </p>
-                <input
-                type="text"
-                placeholder="New password..."
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}/>
-                <button></button>
-            </div>
-            }
+
+            {open && (
+                <div className="model-background" onClick={() => setOpen(false)}>
+                    <div className="model" onClick={(e) => e.stopPropagation()}>
+                        <h2>Zmień hasło</h2>
+                        <p>Obecne hasło: </p>
+                        <input
+                        type="text"
+                        placeholder="Current password..."
+                        value={currentPassword}
+                        onChange={(e) => setCurrentPassword(e.target.value)}/>
+                        <p>Nowe hasło: </p>
+                        <input
+                        type="text"
+                        placeholder="New password..."
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}/>
+                        <button onClick={handleChangePassword}>Zmień</button>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
