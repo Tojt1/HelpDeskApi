8c92e961523903edf4a8c7f77817ac635ec5d2d3
Tojt1 <konsikwojciech@onet.pl>
2026-08-31 01:01:56 +0200

    add style for websites

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,32 +4,7 @@ import Tickets from "../components/Tickets.jsx";
 import "./Dashboard.css"
 
 function Dashboard(){
-    const token = localStorage.getItem("token");
-    const users = jwtDecode(token);
-    const [problemTitle, setProblemTitle] = useState("")
-    const [problemDescription, setProblemDescription] = useState("")
-    const [problemCategory, setProblemCategory] = useState("")
 
-
-    const SubmitDash = async (e) => {
-        e.preventDefault()
-
-        const token = localStorage.getItem("token")
-
-        const response = await fetch("http://localhost:8000/tickets", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorisation": `Bearer ${token}`
-            },
-            body:JSON.stringify({
-                title: problemTitle,
-                description: problemDescription,
-                category: problemCategory
-            })
-        })
-        const data = response.json()
-    }
 
     return(
         <>
