ffe4c097e3869fd86ef86db6b19cb11db72daacd
Tojt1 <konsikwojciech@onet.pl>
2026-08-31 01:01:05 +0200

    create button for creating tickets

--- frontend/src/pages/CreateToken.jsx
+++ frontend/src/pages/CreateToken.jsx
@@ -1,32 +1,69 @@
-import Tickets from "../components/Tickets.jsx";
+import {jwtDecode} from "jwt-decode";
+import {useState} from "react";
 
-<div className="dasboard-container">
+function CreateToken() {
+    const token = localStorage.getItem("token");
+    const users = jwtDecode(token);
+    const [problemTitle, setProblemTitle] = useState("")
+    const [problemDescription, setProblemDescription] = useState("")
+    const [problemCategory, setProblemCategory] = useState("")
 
-                <h2>Hello {users.name}</h2>
 
-                <form onSubmit={SubmitDash} className="dashboard-form">
+    const SubmitDash = async (e) => {
+        e.preventDefault()
 
-                    <p>Jakiego rodzaju jest twój problem?</p>
-                    <input
-                    type="text"
-                    placeholder="....."
-                    value={problemTitle}
-                    onChange={(e) => setProblemTitle(e.target.value)}/>
+        const token = localStorage.getItem("token")
 
-                    <p>Opisz swój problem</p>
-                    <input
-                    type="text"
-                    placeholder="....."
-                    value={problemDescription}
-                    onChange={(e) => setProblemDescription(e.target.value)}/>
+        const response = await fetch("http://localhost:8000/tickets", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorisation": `Bearer ${token}`
+            },
+            body:JSON.stringify({
+                title: problemTitle,
+                description: problemDescription,
+                category: problemCategory
+            })
+        })
+        if (response.ok){
+            alert("Pomyślnie utworzono ticket")
+        }
+    }
 
-                    <p>Z jakiej kategori jest twój problem</p>
-                    <input
-                    type="text"
-                    placeholder="Category...."
-                    value={problemCategory}
-                    onChange={(e)=> setProblemCategory(e.target.value)}/>
 
-                    <button type="submit" className="dashboard-btn">Wyślij ticket</button>
-                </form>
-            </div>
+    return(
+    <div className="dasboard-container">
+
+        <h2>Hello {users.name}</h2>
+
+        <form onSubmit={SubmitDash} className="dashboard-form">
+
+            <p>Jakiego rodzaju jest twój problem?</p>
+            <input
+                type="text"
+                placeholder="....."
+                value={problemTitle}
+                onChange={(e) => setProblemTitle(e.target.value)}/>
+
+            <p>Opisz swój problem</p>
+            <input
+                type="text"
+                placeholder="....."
+                value={problemDescription}
+                onChange={(e) => setProblemDescription(e.target.value)}/>
+
+            <p>Z jakiej kategori jest twój problem</p>
+            <input
+                type="text"
+                placeholder="Category...."
+                value={problemCategory}
+                onChange={(e) => setProblemCategory(e.target.value)}/>
+
+            <button type="submit" className="dashboard-btn">Wyślij ticket</button>
+        </form>
+    </div>
+    )
+}
+
+export default CreateToken
