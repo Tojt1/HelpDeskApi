73ba62abb3317cfe136081090ec5576d4d74271c
Tojt1 <konsikwojciech@onet.pl>
2026-09-08 18:52:03 +0200

    send emails

---
+++ backend/sen_emails.py
@@ -0,0 +1,33 @@
+import resend
+import config
+
+resend.api_key = config.RESEND_API_KEY
+
+def sendWelocme_email(user_email):
+    params = {
+        "from": "onboarding@resend.dev",
+        "to": [user_email],
+        "subject": "Witaj!",
+        "html": """
+        <h1>Witaj w naszej aplikacji </h1>
+        <p>Dziękujemy, że skorzystałeś z naszej aplikacji.</p>
+        <p> Mamy nadziję, że miło będzie ci się z niej korzystało</p>
+        <h2>HelpDesk API</h2>
+        """
+    }
+
+    email = resend.Emails.send(params)
+
+
+def sendDelete_email(user_email):
+    params = {
+        "from": "onboarding@resend.dev",
+        "to": [user_email],
+        "subject": "Przykro nam że już nas opuszczasz",
+        "html": """
+            <h1>Przykro nam, że już nas opuszczas</h1>
+            <p>Siema</p>
+        """
+    }
+
+    resend.Emails.send(params)
--- config.py
+++ config.py
@@ -8,6 +8,6 @@ USER = os.getenv('DB_USER')
 PASSWORD = os.getenv('DB_PASSWORD')
 NAME = os.getenv('DB_NAME')
 
-
+RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 
 SECRET_KEY = os.getenv('SECRET_KEY')
