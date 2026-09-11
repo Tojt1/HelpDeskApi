d6ee87b530b4c216b51b851d9ed82b639a649125
Tojt1 <konsikwojciech@onet.pl>
2026-09-12 00:41:21 +0200

    token expire time in fronted

--- frontend/src/components/CheckLogged.jsx
+++ frontend/src/components/CheckLogged.jsx
@@ -1,10 +1,35 @@
-import {Navigate, Outlet} from "react-router";
+import {useNavigate, Outlet} from "react-router";
+import {jwtDecode} from "jwt-decode";
+import {useEffect} from "react";
 
 function CheckLogged ( {children}) {
+    const navigate = useNavigate()
     const token = localStorage.getItem("token")
+
     if (! token){
-        return <Navigate to="/login"/>
+        navigate("/login")
     }
+    const users = jwtDecode(token)
+
+    const expiretime = users.exp * 1000
+    const timeleft = expiretime - Date.now()
+
+    useEffect(() => {
+        if (timeleft<=0) {
+            localStorage.removeItem("token")
+            navigate("/login")
+        }
+
+        const timer = setTimeout(() => {
+            localStorage.removeItem("token")
+            navigate("/login")
+        }, timeleft)
+
+        return () => clearTimeout(timer)
+
+    }, [timeleft, navigate]);
+
+
     return <Outlet />
 }
 
