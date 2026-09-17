3077c4817ea1ec73ec69400e1d477812533aca19
Tojt1 <konsikwojciech@onet.pl>
2026-09-17 15:54:56 +0200

    Admin layout

---
+++ frontend/src/components/Adminmenu.jsx
@@ -0,0 +1,29 @@
+import {useNavigate} from "react-router";
+import {useState} from "react";
+
+function Adminmenu (){
+    const [open, setOpen] = useState(false)
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem("token")
+        navigate("/")
+    }
+
+    return(
+        <>
+            <div className="amenu-container">
+                <button className="aopen-menu" onClick={()=> setOpen(true)}>👤</button>
+                {open &&(
+                    <div className="amenu-options">
+                        <button className="amenu-tickets" onClick={()=> navigate("/admin/dashboard")}>✉️Ticket</button>
+                        <button className="amenu-settings" onClick={()=> navigate("/admin/settings")}>⚙️ Ustawienia</button>
+                        <button classname="amenu-logout" onClick={handleLogout}>❌ Wyloguj się</button>
+                    </div>
+                )}
+            </div>
+        </>
+    )
+}
+
+export default Adminmenu
